d3509c2b41e8e68ff48a165def340be58828145a
oluwaseun <omoniyi22>
2021-01-05 14:42:06 +0100

    task completed

--- app/utils/bot.js
+++ app/utils/bot.js
@@ -24,45 +24,41 @@ module.exports.check_product_sold = async (product_names, callback) => {
   let page, html, all_sales = []
 
   for (let product_name of product_names) {
-    try {
 
-      let start = new Date().getTime();
-      let product_sales = []
+    let start = new Date().getTime();
+    let product_sales = []
 
-      for (let page_num = 1; page_num <= 4; page_num++) {
+    for (let page_num = 1; page_num <= 4; page_num++) {
 
-        page = await configureBrowser(product_name, (page_num))
-        html = await page.evaluate(() => document.body.innerHTML)
-        await page.reload();
+      page = await configureBrowser(product_name, (page_num))
+      html = await page.evaluate(() => document.body.innerHTML)
+      await page.reload();
 
-        $('.s-item', html).each(function () {
-          let product_sold = {
-            name: $('.s-item__title', this).text().replace(/\\/g, "_"),
-            sold_on: $('.POSITIVE:contains("Sold")', this).text(),
-            price: $('.POSITIVE:contains("$")', this).text(),
-          }
+      $('.s-item', html).each(function () {
+        let product_sold = {
+          name: $('.s-item__title', this).text().replace(/\\/g, "_"),
+          sold_on: $('.POSITIVE:contains("Sold")', this).text(),
+          price: $('.POSITIVE:contains("$")', this).text(),
+        }
 
-          let { name, sold_on, price } = product_sold
-          console.log({ product_sold })
-          if (name && sold_on && price) {
-            product_sales.push(product_sold)
-          }
-        })
-      }
-      let end = new Date().getTime();
-
-      let result_details = {
-        fetched_on: Date().slice(0, 24),
-        product_name,
-        total: product_sales.length,
-        fetch_duration: `${((end - start) / 1000)}sec`,
-      }
-      product_sales.unshift(result_details)
-      all_sales.push(product_sales)
-      await page.close()
-    } catch (error) {
-      console.log('An error ocucured')
+        let { name, sold_on, price } = product_sold
+        if (name && sold_on && price) {
+          product_sales.push(product_sold)
+        }
+      })
     }
+    let end = new Date().getTime();
+
+    let result_details = {
+      fetched_on: Date().slice(0, 24),
+      product_name,
+      total: product_sales.length,
+      fetch_duration: `${((end - start) / 1000)}sec`,
+    }
+    product_sales.unshift(result_details)
+    all_sales.push(product_sales)
   }
+
   callback(all_sales, true)
+  await page.close()
 }
