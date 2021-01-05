5aa3f96afa28a397720788b5465c64c6f8086ca4
oluwaseun <omoniyi22>
2021-01-05 22:05:55 +0100

    task completed

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const { Routes } = require("./routes/index")
 const { ProductFetcher } = require('./services/ProductFetcher')
 const path = require('path')
+require('dotenv').config();
 
 app.use(cors())
 app.use(express.json())
@@ -22,4 +23,13 @@ app.use('/api/v1', router);
 
 app.get('*', Pages)
 
-module.exports = app;
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("App running");
+  }
+});
